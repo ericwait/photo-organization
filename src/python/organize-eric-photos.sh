7a64c26159689bada7629e4e24ee8e1a4a65c4b7
Eric Wait <eric@waitphoto.com>
2020-01-14 20:57:30 -0500

    Added script to run from synology nas

---
+++ src/python/organize-eric-photos.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /volume1/homes/ewait/Documents/programming/photo-organize
+/var/services/homes/ewait/.local/share/virtualenvs/photo-organize-PDu68t8w/bin/python3 organize.py /volume1/photo/Eric\'s\ phone/ /volume1/photo/RAW/ /volume1/photo/eric-reorg
+chmod -R PhotoStation:PhotoStation /volume1/photo/RAW
+
