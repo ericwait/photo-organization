830c3cbd65880f5f31c47ac880c9095e2acb60ae
Eric Wait <eric@waitphoto.com>
2021-02-04 09:03:51 -0500

    Photo backup takes a user name

--- src/python/organize-eric-photos.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /volume1/homes/ewait/Documents/programming/photo-organize
-/var/services/homes/ewait/.local/share/virtualenvs/photo-organize-PDu68t8w/bin/python3 organize.py /volume1/homes/ewait/Drive/Moments/Mobile/ /volume1/photo/RAW/
-chown -R PhotoStation:PhotoStation /volume1/photo/RAW
-
--- src/python/organize-photos.sh
+++ src/python/organize-photos.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 cd /volume1/homes/ewait/Documents/programming/photo-organize
-/var/services/homes/ewait/.local/share/virtualenvs/photo-organize-PDu68t8w/bin/python3 organize.py /volume1/photo-no-index/camera-upload /volume1/photo/RAW/
-chown -R PhotoStation:PhotoStation /volume1/photo/RAW
+# source /volume1/homes/ewait/py-all/bin/activate
+python3 organize.py /volume1/homes/$1/Drive/Moments/Mobile/ /volume1/photo/RAW/
+find /volume1/homes/$1/Drive/Moments/Mobile -name "@eaDir" -exec rm -rf '{}' \;
+find /volume1/homes/$1/Drive/Moments/Mobile -type d -empty -delete
