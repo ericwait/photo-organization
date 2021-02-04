f82b1b0b5d86c742fbd83fd74a13e87e9f4b5239
Eric Wait <eric@waitphoto.com>
2021-02-04 09:04:49 -0500

    Changed api key location

--- src/python/location.py
+++ src/python/location.py
@@ -2,19 +2,18 @@
 import requests
 import json
 
-def get_closest_city(lat, long):
+def get_closest_city(lat, long, google_api_key_path='api_key.txt'):
 # %%
-    api_key_file = open("../../api_key.txt",'r')
+    api_key_file = open(google_api_key_path,'r')
     api_key = api_key_file.readline()[:-1]
     api_key_file.close()
 
-    lat = 40.714224
-    long = -73.714224
+    # lat = 40.714224
+    # long = -73.714224
 
     url = f'https://maps.googleapis.com/maps/api/geocode/json?latlng={lat},{long}&key={api_key}'
 
     rev_geocode = requests.get(url)
-    rev_geocode_json = json.loads(rev_geocode.json())
 
     first_address = rev_geocode.json()['results'][0]['address_components']
 
